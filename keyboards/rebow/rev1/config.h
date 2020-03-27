c2160b598d5c32867b2481e080ad60dbe84adb49
Philipp Krenz <krenz@wsoptics.de>
2020-03-27 17:49:18 +0100

    Fix pins

--- keyboards/rebow/rev1/config.h
+++ keyboards/rebow/rev1/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // wiring of each half
 #define MATRIX_ROW_PINS \
-    { D4, D7, E6, B4, B5 }
+    { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS \
     { F5, F6, F7, B1, B3, B2, B6 }
 // #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5} //uncomment this line and comment line above if you need to reverse left-to-right key order
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBOUNCE 5
 
 /* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D0
+// #define SOFT_SERIAL_PIN D0
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
